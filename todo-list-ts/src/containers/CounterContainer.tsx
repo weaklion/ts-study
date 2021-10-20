8c0365fe6ccb149118819919670dd2fd7f621ab7
xii-gyuhan <kh.lee@xiilab.com>
2021-10-20 17:25:07 +0900

    counter 작성

---
+++ todo-list-ts/src/containers/CounterContainer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../modules';
+import { increase, decrease, increaseBy } from "../modules/counter";
+import Counter from '../components/Counter';
+
+const CounterContainer = () => {
+
+  const count = useSelector((state: RootState) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => {
+    dispatch(increase());
+  };
+
+  const onDecrease = () => {
+    dispatch(decrease());
+  };
+
+  const onIncreaseBy = (diff: number) => {
+    dispatch(increaseBy(diff));
+  };
+
+  return (
+    <Counter
+      count={count}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+      onIncreaseBy={onIncreaseBy}
+    />
+  );
+};
+
+export default CounterContainer;
