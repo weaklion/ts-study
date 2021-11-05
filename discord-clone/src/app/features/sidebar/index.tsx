12ba878354029c73abd5a1eeb37ce1c071d8f7b2
weaklion <weaklion@naver.com>
2021-11-05 13:53:53 +0900

    discord-clone 수정

---
+++ discord-clone/src/app/features/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>I am the Sidebar</h2>
+    </div>
+  )
+}
+
+export default Sidebar;
