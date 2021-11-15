c2b6c926ed076596dc17ad46845c30c85b6cd17e
weaklion <weaklion@naver.com>
2021-11-15 15:23:19 +0900

    req 추가, res 추가

--- chapter/vanila-SPA/app.ts
+++ chapter/vanila-SPA/app.ts
@@ -6,7 +6,20 @@ const app = express();
 app.use(
   "/static",
   express.static(path.resolve(__dirname, "frontend", "src"))
-)
+);
+
+app.get("/*", (req : express.Request, res : express.Response) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
+});
+
+// app.get("/about", (req : express.Request, res : express.Response) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
+// });
+
+
+// app.get("/about:id", (req : express.Request, res : express.Response) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
+// });
 
 
 
