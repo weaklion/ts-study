b59f774cadf90b50a47ca132c0d07ff9e161db9c
gyuhan Lee <weaklion4997@gmail.com>
2021-11-17 11:25:38 +0900

    index.ts 수정

--- chapter/vanila-SPA/frontend/src/index.ts
+++ chapter/vanila-SPA/frontend/src/index.ts
@@ -1,13 +1,18 @@
 
 
 
+const navigateTo = (url : string) => {
+  history.pushState(null, null, url);
+}
+
 const router = async () => {
+
   const routes = [
     { path : "/", view : "Dashboard" },
     { path : "/:id", view : "Detail" },
   ]
 
-  const routeMatches = routes.map((route) => {
+  const routeMatches = routes.map((route)  => {
     return {
       route : route,
       isMatch : location.pathname === route.path
@@ -26,3 +31,14 @@ const router = async () => {
   }
 }
 
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e : MouseEvent) => {
+    if((<HTMLElement> e.target).matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo((<HTMLLIElement> e.target).getAttribute('href'));
+    }
+  });
+  router();
+})
